4b452705acdea6f929b150fa5ecf87ec22db6d94
Aditya Bhushan Sharma <75534658+aditya270520@users.noreply.github.com>
2021-07-25 10:20:23 +0530

    Add files via upload

--- STORE-INFORMATION-OF-STUDENT-USING-STRUCTURE.c
+++ STORE-INFORMATION-OF-STUDENT-USING-STRUCTURE.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 struct student {
-    char name[50];
+    char firstName[50];
     int roll;
     float marks;
-} s;
+} s[10];
 
 int main() {
-    printf("Enter information:\n");
-    printf("Enter name: ");
-    fgets(s.name, sizeof(s.name), stdin);
+    int i;
+    printf("Enter information of students:\n");
 
-    printf("Enter roll number: ");
-    scanf("%d", &s.roll);
-    printf("Enter marks: ");
-    scanf("%f", &s.marks);
+    // storing information
+    for (i = 0; i < 5; ++i) {
+        s[i].roll = i + 1;
+        printf("\nFor roll number%d,\n", s[i].roll);
+        printf("Enter first name: ");
+        scanf("%s", s[i].firstName);
+        printf("Enter marks: ");
+        scanf("%f", &s[i].marks);
+    }
+    printf("Displaying Information:\n\n");
 
-    printf("Displaying Information:\n");
-    printf("Name: ");
-    printf("%s", s.name);
-    printf("Roll number: %d\n", s.roll);
-    printf("Marks: %.1f\n", s.marks);
-
+    // displaying information
+    for (i = 0; i < 5; ++i) {
+        printf("\nRoll number: %d\n", i + 1);
+        printf("First name: ");
+        puts(s[i].firstName);
+        printf("Marks: %.1f", s[i].marks);
+        printf("\n");
+    }
     return 0;
 }
